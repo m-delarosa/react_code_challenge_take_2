63c1e293dab4517966ef0909a1d4a20cecc97d44
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-06 21:02:10 -0600

    Discharge a bot from the frontend by clicking the red X.

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -4,7 +4,7 @@ import BotCard from "../components/BotCard"
 export default function YourBotArmy(props) {
 
   const displayBots = props.bots.map(bot => {
-    return <BotCard bot={bot} action={props.action} />
+    return <BotCard bot={bot} action={props.action} removeCard={props.removeCard} />
   })
 
 
