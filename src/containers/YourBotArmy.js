96ab1da9100e39bcc6a7d33bf0e35bb599931c68
github username <github email address>
2020-06-06 20:40:00 -0600

    Release a bot from YourBotArmy by clicking on it.

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react"
+import React from 'react'
 import BotCard from "../components/BotCard"
 
 export default function YourBotArmy(props) {
 
   const displayBots = props.bots.map(bot => {
-    return <BotCard bot={bot} />
+    return <BotCard bot={bot} action={props.action} />
   })
 
 
