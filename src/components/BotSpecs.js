cce2ff2f5efbe282fb49a75a861c1a4d45164929
Michael De La Rosa <56710845+m-delarosa@users.noreply.github.com>
2020-06-07 22:32:03 -0600

    Add enlist button functionality to BotSpecs component.

--- src/components/BotSpecs.js
+++ src/components/BotSpecs.js
@@ -61,9 +61,7 @@ const BotSpecs = props => {
             <button
               className="ui button fluid"
               onClick={() =>
-                console.log(
-                  "connect this to a function that adds this bot to your bot army list"
-                )
+                props.enlist(props.bot)
               }
             >
               Enlist
